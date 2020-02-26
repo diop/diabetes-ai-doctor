72e9e2becdd37addc958869833e8baf79fd28dcc
diop <fodesdiop@gmail.com>
2020-02-26 15:27:58 +0000

    add fontawesome icons for male and female

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -21,10 +22,22 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: <Widget>[
               Expanded(
-                child: ReusableCard(colour: activeCardColor,),
+                child: ReusableCard(
+                  colour: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'Male',
+                  ),
+                ),
               ),
               Expanded(
-                child: ReusableCard(colour: activeCardColor,),
+                child: ReusableCard(
+                  colour: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'Female',
+                  )
+                ),
               )
             ],
           )),
@@ -53,14 +66,42 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+
+  IconContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(label, style: TextStyle(
+          fontSize: 18.0, color: Color(0xFF8D8E98)
+        ),)
+      ],);
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
